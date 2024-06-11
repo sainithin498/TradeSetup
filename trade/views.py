8e6fdc1a2828af8fd32333be39d9c99aeab052b4
Eswar <eswar.m@dvara.com>
2024-06-11 20:02:05 +0530

    Updated buyorder

--- trade/views.py
+++ trade/views.py
@@ -17,9 +17,11 @@ def roundnearest(val, _type):
     #     res = 100 + val-prcn
     if _type == 'BUY':
         res = val-prcn
+        code = 'CE'
     else:
         res = 100 + val-prcn
-    return res
+        code = 'PE'
+    return res, code
 
 def dategeneration(weekday):
     today = datetime.datetime.now().date()
@@ -30,14 +32,7 @@ def dategeneration(weekday):
     year, month, date = resDt.year, resDt.month, resDt.day
     return year, month, date 
 
-
-@api_view(["GET", "POST"])
-def buyOrder(request):
-    # import ipdb ; ipdb.set_trace()
-    print('body-----------------------', request.body)
-    jsonData = json.loads(request.body)
-    spot = jsonData.get('price')
-    index = jsonData.get('index')
+def getStrikePrice(spot, index, _type):
     """NSE:NIFTY2292217000CE"""
     if index == 'NIFTY':
         weekday = 3
@@ -46,9 +41,20 @@ def buyOrder(request):
         weekday = 2
         qty = 15
     year, month, date = dategeneration(weekday)
-    strike = "NSE:" + index.upper() + str(year)[2:] + str(month) + str(date)+ str(roundnearest(spot, 'BUY')) + "CE"
+    value, code = roundnearest(int(spot), _type)
+    strike = "NSE:" + index.upper() + str(year)[2:] + str(month) + str(date)+ str(value) + code
+    return strike, qty
 
 
+@api_view(["GET", "POST"])
+def buyOrder(request):
+    # import ipdb ; ipdb.set_trace()
+    print('body-----------------------', request.body)
+    jsonData = json.loads(request.body)
+    spot = jsonData.get('price')
+    index = jsonData.get('index')
+   
+    strike, qty = getStrikePrice(spot, index, 'BUY')
 
     eshwar_data = {
     "symbol":strike,
@@ -63,7 +69,7 @@ def buyOrder(request):
     "offlineOrder":True,
     "orderTag":"tag1"
     }
-    
+    print(eshwar_data)
     eshwar_id = "ISORT89TOC-100"
     token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MTgxMTI1NzYsImV4cCI6MTcxODE1MjIzNiwibmJmIjoxNzE4MTEyNTc2LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWFGRkFPa1huQndhTm1GYkJPa2Z6VmU5LTlXaS00bWZnWXF3Z2lmOHhCa1RCNVdXamRMMTdsZWZWRnFRbWhaQklVU0JYQ21DSEttWWpoeU1uUW1DdVNBM3RXUVdNekh3VlRnQ0ltSVV0LWhrcHVyTT0iLCJkaXNwbGF5X25hbWUiOiJNQVZVUlUgRVNXQVIgUkFPIiwib21zIjoiSzEiLCJoc21fa2V5IjoiMmUxNmJmOGMzNzQ0MTE2OGZjNjQ1MWMyZWEyMjgzNGQxNWUzNTY3ZWM4YmE5MmNjM2RlYTAwMDkiLCJmeV9pZCI6IlhNMTgyNDYiLCJhcHBUeXBlIjoxMDAsInBvYV9mbGFnIjoiTiJ9.tKCpN13w95dJe2t2MROTvcP1FKsDozBpSJc0bumIOmo"
     eshwar = fyersModel.FyersModel(client_id=eshwar_id, token=token, log_path="")
@@ -80,13 +86,19 @@ def buyOrder(request):
 
 @api_view(["GET", "POST"])
 def sellOrder(request):
+    print('body-----------------------', request.body)
+    jsonData = json.loads(request.body)
+    spot = jsonData.get('price')
+    index = jsonData.get('index')
+   
+    strike, qty = getStrikePrice(spot, index, "SELL")
     eshwar_data = {
-    "symbol":"NSE:YESBANK-EQ",
-    "qty":1,
-    "type":1,
-    "side":-1,
-    "productType":"CNC",
-    "limitPrice":25,
+    "symbol": strike,
+    "qty":qty,
+    "type":2,
+    "side":1,
+    "productType":"MARGIN",
+    "limitPrice":0,
     "stopPrice":0,
     "validity":"DAY",
     "disclosedQty":0,
@@ -95,7 +107,7 @@ def sellOrder(request):
     }
     
     eshwar_id = "ISORT89TOC-100"
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MTc0MzQ1ODksImV4cCI6MTcxNzQ2MTA0OSwibmJmIjoxNzE3NDM0NTg5LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbVhmamRlY2NUb1VQYlREd1pDa2tWZ1RBUkQ5QU5jN1d2WUxrak43bmpWN3ZLME52ZG53ZjVEQWQ4OHZxM0JVS3lkU2JZY1c1aTliZlIwbElLaFZaQk96SjlYWmE3cWF0SVBrVnNVZHM2ZGFsM0FXcz0iLCJkaXNwbGF5X25hbWUiOiJNQVZVUlUgRVNXQVIgUkFPIiwib21zIjoiSzEiLCJoc21fa2V5IjoiMmUxNmJmOGMzNzQ0MTE2OGZjNjQ1MWMyZWEyMjgzNGQxNWUzNTY3ZWM4YmE5MmNjM2RlYTAwMDkiLCJmeV9pZCI6IlhNMTgyNDYiLCJhcHBUeXBlIjoxMDAsInBvYV9mbGFnIjoiTiJ9.Ec_9VfMtgPAd5vvFRnOnrEKBAEskg7jwmRn9Madi1SI"
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MTgxMTI1NzYsImV4cCI6MTcxODE1MjIzNiwibmJmIjoxNzE4MTEyNTc2LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWFGRkFPa1huQndhTm1GYkJPa2Z6VmU5LTlXaS00bWZnWXF3Z2lmOHhCa1RCNVdXamRMMTdsZWZWRnFRbWhaQklVU0JYQ21DSEttWWpoeU1uUW1DdVNBM3RXUVdNekh3VlRnQ0ltSVV0LWhrcHVyTT0iLCJkaXNwbGF5X25hbWUiOiJNQVZVUlUgRVNXQVIgUkFPIiwib21zIjoiSzEiLCJoc21fa2V5IjoiMmUxNmJmOGMzNzQ0MTE2OGZjNjQ1MWMyZWEyMjgzNGQxNWUzNTY3ZWM4YmE5MmNjM2RlYTAwMDkiLCJmeV9pZCI6IlhNMTgyNDYiLCJhcHBUeXBlIjoxMDAsInBvYV9mbGFnIjoiTiJ9.tKCpN13w95dJe2t2MROTvcP1FKsDozBpSJc0bumIOmo"
     eshwar = fyersModel.FyersModel(client_id=eshwar_id, token=token, log_path="")
     try:
         eshwar_response = eshwar.place_order(data=eshwar_data)
