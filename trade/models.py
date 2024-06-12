c82544c327edaf56b6d14d5e87e5410f872e8da5
Eswar <eswar.m@dvara.com>
2024-06-12 17:16:57 +0530

    Updated model

--- trade/models.py
+++ trade/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class TradeUser(models.Model):
     trader_name = models.CharField(max_length=50)
     fyer_token = models.TextField()
-    fyer_key = models.CharField(max_length=50)
+    fyer_key = models.CharField(max_length=50, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
+    mobile = models.CharField(unique=True)
+    is_active = models.BooleanField(default=True)
+    balance = models.IntegerField(default=0)
+    token_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.trader_name, self.mobile, self.is_active, self.balance
 
