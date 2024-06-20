2fbad982c35faec63cee7d548aa7bf4d8467a0ea
Eswar <eswar.m@dvara.com>
2024-06-21 00:34:31 +0530

    Updated celry

--- TradeSetup/settings.py
+++ TradeSetup/settings.py
@@ -124,12 +124,9 @@ USE_TZ = True
 
 # Celery settings
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
